ef9f66d78e9f3af5704a25669757baa5ebfb0a12
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-01 23:27:15 -0300

    agregue el footer y el background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-
   return (
     <>
- 
+      <Container className="my-4 mainPage">
+        <h1 className="text-center">Bienvenido</h1>
+      </Container>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
